426096bc271ec8a7e93ea3101326c679f5fc098e
Jared Lander <info@jaredlander.com>
2019-08-10 02:05:33 -0400

    Showed lm

--- code/tidymodels.r
+++ code/tidymodels.r
@@ -21,3 +21,21 @@ str(man_split, max.level=1)
 
 man_train <- training(man_split)
 man_test <- testing(man_split)
+
+library(recipes)
+
+
+base_formula <- TotalValue ~ FireService + 
+    ZoneDist1 + ZoneDist2 + 
+    Class + OwnerType + 
+    LotArea + BldgArea + ComArea + ResArea + 
+    OfficeArea + RetailArea + 
+    NumFloors + UnitsTotal + UnitsRes
+
+mod1 <- lm(base_formula, data=man_train)
+mod1
+summary(mod1)
+
+library(coefplot)
+coefplot(mod1, sort='magnitude')
+coefplot(mod1, sort='magnitude', lwdOuter=0.5)
