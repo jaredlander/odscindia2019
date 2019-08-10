d906c1dedd4755fd51975868443bb64c23b5f1be
Jared Lander <info@jaredlander.com>
2019-08-10 03:23:02 -0400

    Prepped matrices

--- code/tidymodels.r
+++ code/tidymodels.r
@@ -39,3 +39,51 @@ summary(mod1)
 library(coefplot)
 coefplot(mod1, sort='magnitude')
 coefplot(mod1, sort='magnitude', lwdOuter=0.5)
+
+favecolors <- c('blue', 'red', 'blue', 'green', 'blue', 
+                'red', 'blue', 'green', 'red', 
+                'blue', 'blue', 'green', 'red', 'blue',
+                'yellow', 'blue', 'yellow', 'yellow')
+favecolors
+
+model.matrix( ~ favecolors)
+
+base_recipe <- recipe(base_formula, data=man_train)
+base_recipe
+
+man_recipe <- base_recipe %>% 
+    step_zv(all_predictors()) %>% 
+    step_knnimpute(everything(), neighbors=5) %>% 
+    step_log(TotalValue, base=2) %>% 
+    # step_normalize(all_numeric(), -TotalValue) %>% 
+    step_center(all_numeric(), -TotalValue) %>% 
+    step_scale(all_numeric(), -TotalValue) %>% 
+    step_other(all_nominal(), threshold=0.2) %>% 
+    step_dummy(all_nominal(), one_hot=TRUE)
+man_recipe
+
+
+man_prepped <- man_recipe %>% 
+    prep()
+man_prepped
+
+man_x_train <- man_prepped %>% 
+    bake(all_predictors(), 
+         new_data=man_train,
+         composition='dgCMatrix')
+head(man_x_train)
+
+man_y_train <- man_prepped %>% 
+    bake(all_outcomes(),
+         new_data=man_train,
+         composition='matrix')
+head(man_y_train)
+
+man_x_test <- man_prepped %>% 
+    bake(all_predictors(),
+         new_data=man_test,
+         composition='matrix')
+man_y_test <- man_prepped %>% 
+    bake(all_outcomes(),
+         new_data=man_test,
+         composition='matrix')
